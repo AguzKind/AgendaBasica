064d19e6fb34c32f889ac21699f3a289672e387b
Agustin Macoggi <105683402+AguzKind@users.noreply.github.com>
2023-05-08 17:23:31 -0300

    Carga de archivos

---
+++ Agenda basica/Agenda/Form1.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmb_tipocel = new System.Windows.Forms.ComboBox();
+            this.txt_localidad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_dni = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_telefono = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbn_otro = new System.Windows.Forms.RadioButton();
+            this.rbn_fem = new System.Windows.Forms.RadioButton();
+            this.rbn_masc = new System.Windows.Forms.RadioButton();
+            this.dgv_leer = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_leer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Direccion: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Telefono:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.cmb_tipocel);
+            this.groupBox1.Controls.Add(this.txt_localidad);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txt_dni);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_telefono);
+            this.groupBox1.Controls.Add(this.txt_direccion);
+            this.groupBox1.Controls.Add(this.txt_apellido);
+            this.groupBox1.Controls.Add(this.txt_nombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(473, 238);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos:";
+            // 
+            // cmb_tipocel
+            // 
+            this.cmb_tipocel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_tipocel.FormattingEnabled = true;
+            this.cmb_tipocel.Location = new System.Drawing.Point(277, 207);
+            this.cmb_tipocel.Name = "cmb_tipocel";
+            this.cmb_tipocel.Size = new System.Drawing.Size(180, 21);
+            this.cmb_tipocel.TabIndex = 12;
+            // 
+            // txt_localidad
+            // 
+            this.txt_localidad.Location = new System.Drawing.Point(72, 171);
+            this.txt_localidad.Name = "txt_localidad";
+            this.txt_localidad.Size = new System.Drawing.Size(385, 20);
+            this.txt_localidad.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Localidad:";
+            // 
+            // txt_dni
+            // 
+            this.txt_dni.Location = new System.Drawing.Point(72, 25);
+            this.txt_dni.Name = "txt_dni";
+            this.txt_dni.Size = new System.Drawing.Size(385, 20);
+            this.txt_dni.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DNI:";
+            // 
+            // txt_telefono
+            // 
+            this.txt_telefono.Location = new System.Drawing.Point(72, 207);
+            this.txt_telefono.Name = "txt_telefono";
+            this.txt_telefono.Size = new System.Drawing.Size(188, 20);
+            this.txt_telefono.TabIndex = 6;
+            this.txt_telefono.TextChanged += new System.EventHandler(this.txt_telefono_TextChanged);
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(72, 134);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(385, 20);
+            this.txt_direccion.TabIndex = 4;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(72, 97);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(385, 20);
+            this.txt_apellido.TabIndex = 3;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(72, 61);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(385, 20);
+            this.txt_nombre.TabIndex = 2;
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.BackColor = System.Drawing.Color.Silver;
+            this.btn_Agregar.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Agregar.ForeColor = System.Drawing.Color.Black;
+            this.btn_Agregar.Location = new System.Drawing.Point(12, 323);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(405, 35);
+            this.btn_Agregar.TabIndex = 7;
+            this.btn_Agregar.Text = "Agregar Usuario";
+            this.btn_Agregar.UseVisualStyleBackColor = false;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.rbn_otro);
+            this.groupBox2.Controls.Add(this.rbn_fem);
+            this.groupBox2.Controls.Add(this.rbn_masc);
+            this.groupBox2.Location = new System.Drawing.Point(12, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(473, 69);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sexo:";
+            // 
+            // rbn_otro
+            // 
+            this.rbn_otro.AutoSize = true;
+            this.rbn_otro.Location = new System.Drawing.Point(360, 28);
+            this.rbn_otro.Name = "rbn_otro";
+            this.rbn_otro.Size = new System.Drawing.Size(45, 17);
+            this.rbn_otro.TabIndex = 2;
+            this.rbn_otro.TabStop = true;
+            this.rbn_otro.Text = "Otro";
+            this.rbn_otro.UseVisualStyleBackColor = true;
+            // 
+            // rbn_fem
+            // 
+            this.rbn_fem.AutoSize = true;
+            this.rbn_fem.Location = new System.Drawing.Point(189, 28);
+            this.rbn_fem.Name = "rbn_fem";
+            this.rbn_fem.Size = new System.Drawing.Size(71, 17);
+            this.rbn_fem.TabIndex = 1;
+            this.rbn_fem.TabStop = true;
+            this.rbn_fem.Text = "Femenino";
+            this.rbn_fem.UseVisualStyleBackColor = true;
+            // 
+            // rbn_masc
+            // 
+            this.rbn_masc.AutoSize = true;
+            this.rbn_masc.Location = new System.Drawing.Point(27, 28);
+            this.rbn_masc.Name = "rbn_masc";
+            this.rbn_masc.Size = new System.Drawing.Size(73, 17);
+            this.rbn_masc.TabIndex = 0;
+            this.rbn_masc.TabStop = true;
+            this.rbn_masc.Text = "Masculino";
+            this.rbn_masc.UseVisualStyleBackColor = true;
+            // 
+            // dgv_leer
+            // 
+            this.dgv_leer.AllowUserToAddRows = false;
+            this.dgv_leer.AllowUserToDeleteRows = false;
+            this.dgv_leer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_leer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column8,
+            this.Column7});
+            this.dgv_leer.Location = new System.Drawing.Point(12, 364);
+            this.dgv_leer.Name = "dgv_leer";
+            this.dgv_leer.ReadOnly = true;
+            this.dgv_leer.RowHeadersVisible = false;
+            this.dgv_leer.Size = new System.Drawing.Size(473, 139);
+            this.dgv_leer.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "DNI";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Sexo";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nombre";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Apellido";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Direccion";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Localidad";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Tipo";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Telefono";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.Color.Red;
+            this.btn_salir.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(423, 323);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(61, 35);
+            this.btn_salir.TabIndex = 8;
+            this.btn_salir.Text = "X";
+            this.btn_salir.UseVisualStyleBackColor = false;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Agenda.Properties.Resources.Desert;
+            this.ClientSize = new System.Drawing.Size(496, 523);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.dgv_leer);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_Agregar);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_leer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_telefono;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.TextBox txt_dni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_localidad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbn_fem;
+        private System.Windows.Forms.RadioButton rbn_masc;
+        private System.Windows.Forms.ComboBox cmb_tipocel;
+        private System.Windows.Forms.DataGridView dgv_leer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.RadioButton rbn_otro;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ Agenda basica/Agenda/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CargarGrilla()
+        {
+            dgv_leer.Rows.Clear();
+            StreamReader L = new StreamReader("C:\\Agenda.txt");
+            int linea = 0; //Declaro contador para lineas o filas de la grilla
+            for (; ; )
+            {
+                
+                string s = L.ReadLine(); //Es una variable de tipo string cargo lo que leo del archivo (la fila)
+                if (s == null) break; //Si es fin de archivo termino el bucle
+                string[] array = s.Split(';'); //Declaro y cargo un array con el contenido de la fila leida separando datos por ;
+                dgv_leer.Rows.Add(); //Agrego una fila en blanco a la grilla
+                dgv_leer[0, linea].Value = array[0];
+                dgv_leer[1, linea].Value = array[1];
+                dgv_leer[2, linea].Value = array[2];
+                dgv_leer[3, linea].Value = array[3];
+                dgv_leer[4, linea].Value = array[4];
+                dgv_leer[5, linea].Value = array[5];
+                dgv_leer[6, linea].Value = array[6];
+                dgv_leer[7, linea].Value = array[7];
+                linea = linea + 1; //Aumento en 1 la cantidad de filas del contador
+            }
+            dgv_leer.ClearSelection();
+            L.Close();
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+            if ((txt_dni.Text != "") && (txt_apellido.Text != "") && (txt_nombre.Text != "") && (txt_localidad.Text != "") && (txt_telefono.Text != "") && (txt_direccion.Text != ""))
+            {
+            try
+            {
+                string sexo = "";
+                if (rbn_masc.Checked == true)
+                {
+                    sexo = "Masculino";
+                }
+                else if (rbn_fem.Checked == true) sexo = "Femenino";
+                else if (rbn_otro.Checked == true) sexo = "Otro";
+
+                string fileName = "C:\\Agenda.txt";
+                FileStream stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream);
+                writer.WriteLine(txt_dni.Text + ";" + sexo + ";" + txt_nombre.Text + ";" + txt_apellido.Text + ";" + txt_direccion.Text + ";" + txt_localidad.Text + ";" + cmb_tipocel.Text + ";" + txt_telefono.Text + ";");
+                writer.Close();
+                MessageBox.Show("La persona fue agregada correctamente", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txt_dni.Text = "";
+                txt_nombre.Text = "";
+                txt_apellido.Text = "";
+                txt_direccion.Text = "";
+                txt_localidad.Text = "";
+                txt_telefono.Text = "";
+                rbn_masc.Checked = true;
+                cmb_tipocel.SelectedIndex = 0;
+                txt_dni.Focus();
+                CargarGrilla();
+            }
+            catch
+            {
+                MessageBox.Show("Error en la carga de los datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            } else MessageBox.Show("Por favor, cargue todos los datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+        }
+
+        private void txt_telefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programa realizado por Agustin Macoggi", "Bienvenido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            rbn_masc.Checked = true;
+            cmb_tipocel.Items.Add("Fijo");
+            cmb_tipocel.Items.Add("Celular");
+            cmb_tipocel.SelectedIndex = 0;
+            CargarGrilla();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Esta seguro que desea salir?", "Atencion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                dialogResult = DialogResult.No;
+            }
+        }
+    }
+}
